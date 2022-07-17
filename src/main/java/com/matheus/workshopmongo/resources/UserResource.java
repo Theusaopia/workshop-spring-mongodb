f0aa13881f3f14a2aca34ac723e2f25812f338c9
Matheus Dias <matheusdyas007@hotmail.com>
2022-07-17 11:20:54 -0300

    implementing dto pattern for users

--- src/main/java/com/matheus/workshopmongo/resources/UserResource.java
+++ src/main/java/com/matheus/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.matheus.workshopmongo.resources;
 
 import com.matheus.workshopmongo.domain.User;
+import com.matheus.workshopmongo.dto.UserDTO;
 import com.matheus.workshopmongo.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,9 +22,9 @@ public class UserResource {
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-
-        return ResponseEntity.ok(list);
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok(listDto);
     }
 }
