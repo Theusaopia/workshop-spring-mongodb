fc1f618e20549ce0a20068e45bfe0d5f8f514565
Matheus Dias <matheusdyas007@hotmail.com>
2022-07-17 11:36:32 -0300

    implementing findById endpoint for user

--- src/main/java/com/matheus/workshopmongo/services/UserService.java
+++ src/main/java/com/matheus/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.matheus.workshopmongo.services;
 
 import com.matheus.workshopmongo.domain.User;
 import com.matheus.workshopmongo.repositories.UserRepository;
+import com.matheus.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
