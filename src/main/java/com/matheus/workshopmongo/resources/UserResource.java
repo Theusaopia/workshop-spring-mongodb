9a87e47e2cf669e0c86fb26e03a9a5c5bd570c85
Matheus Dias <matheusdyas007@hotmail.com>
2022-07-17 10:21:16 -0300

    added DI on the user resource

--- src/main/java/com/matheus/workshopmongo/resources/UserResource.java
+++ src/main/java/com/matheus/workshopmongo/resources/UserResource.java
@@ -1,25 +1,27 @@
 package com.matheus.workshopmongo.resources;
 
 import com.matheus.workshopmongo.domain.User;
+import com.matheus.workshopmongo.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    private final UserService service;
+
+    public UserResource(UserService service) {
+        this.service = service;
+    }
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria Silva", "maria@email.com");
-        User alex = new User("2", "Alex", "Alex@email.com");
-
-        List<User> list = new ArrayList<>(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();
 
         return ResponseEntity.ok(list);
     }
