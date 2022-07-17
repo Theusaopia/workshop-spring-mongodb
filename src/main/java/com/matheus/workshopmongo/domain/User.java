ec8346e32ec02a9e4923dd04dcb22c512defe5f8
Matheus Dias <matheusdyas007@hotmail.com>
2022-07-17 10:21:52 -0300

    connecting with mongoDB and user annotations

--- src/main/java/com/matheus/workshopmongo/domain/User.java
+++ src/main/java/com/matheus/workshopmongo/domain/User.java
@@ -1,10 +1,17 @@
 package com.matheus.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document
 public class User implements Serializable {
+
+    @Id
     private String id;
+
     private String name;
     private String email;
 
