0fd65fdc4251ebae8f809853973800e6712f1fa8
Matheus Dias <matheusdyas007@hotmail.com>
2022-07-18 20:49:13 -0300

    creating post entity

--- src/main/java/com/matheus/workshopmongo/config/Instantiation.java
+++ src/main/java/com/matheus/workshopmongo/config/Instantiation.java
@@ -1,23 +1,32 @@
 package com.matheus.workshopmongo.config;
 
+import com.matheus.workshopmongo.domain.Post;
 import com.matheus.workshopmongo.domain.User;
+import com.matheus.workshopmongo.repositories.PostRepository;
 import com.matheus.workshopmongo.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
     private final UserRepository repository;
+    private final PostRepository postRepository;
 
-    public Instantiation(UserRepository repository) {
+    public Instantiation(UserRepository repository, PostRepository postRepository) {
         this.repository = repository;
+        this.postRepository = postRepository;
     }
 
     @Override
-    public void run(String... args){
+    public void run(String... args) throws Exception{
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         repository.deleteAll();
 
@@ -25,6 +34,10 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar pro rio", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Bom dia geral do grupo torcida jovem", alex);
+
         repository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
